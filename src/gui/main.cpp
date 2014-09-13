602fd7d71f4628295bbd844e1f5216d4115d3a52
Vyacheslav Koshcheyev <vok1980@gmail.com>
2014-09-13 23:49:44 +0700

    add custom log handler

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,15 +1,33 @@
 
 #include "mainwindow.h"
+
+#include <iostream>
 #include <QApplication>
 
 #include "timerecordconrol.h"
 
 
+
+void msgOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    const char chTypeSymbols[] = { 'I', 'W', 'E', 'X' };
+    QByteArray localMsg = msg.toLocal8Bit();
+
+    fprintf(stderr, "[%c]: %s (%s:%u, %s)\n", chTypeSymbols[type], localMsg.constData(), context.file, context.line, context.function);
+
+    if( type == QtFatalMsg )
+        abort();
+}
+
+
+
 int main(int argc, char *argv[])
 {
-    std::tr1::shared_ptr<TimeRecordConrol> pTrc( new TimeRecordConrol );
+    qInstallMessageHandler(msgOutput);
 
+    std::tr1::shared_ptr<TimeRecordConrol> pTrc( new TimeRecordConrol );
     QApplication a(argc, argv);
+
     MainWindow w(NULL, pTrc);
     w.show();
 
